dfb3264464611113e13e5beca0ea09adbf3c4ae7
Ramprasad <ramprasad6352@gmail.com>
2024-04-20 12:46:07 +0530

    Post and getMapping

---
+++ src/main/java/com/accio/Hospital/Management/System/ApiClass.java
@@ -0,0 +1,51 @@
+package com.accio.Hospital.Management.System;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
+public class ApiClass {
+
+    HashMap<Integer,Patient> patientDb = new HashMap<>();
+
+    @GetMapping("getReport")
+    public static String getWeatherUpdates(){
+        return "the temp is 100 degree now";
+    }
+
+    @GetMapping("addition")
+    public static String add(@RequestParam("n1") int n1,@RequestParam("n2") int n2,@RequestParam("operator") char ch){
+        if(ch=='+'){
+            int sum = n1+n2;
+            return "the sum is "+sum;
+        }
+        else if(ch=='-'){
+            int sub = n1-n2;
+            return "the sub is "+sub;
+        }
+        else if(ch=='/') {
+            int div = n1/n2;
+            return "the div is " +div;
+        }
+        else{
+            int mul = n1*n2;
+            return "the mul is "+mul;
+        }
+
+    }
+
+    @PostMapping("addingPatientInfo")
+    public String addPatient(@RequestBody Patient patient){
+        int key = patient.getPatientId();
+        patientDb.put(key,patient);
+        return "patient added successfully";
+    }
+
+    @GetMapping("getPatientInfo")
+
+    public Patient getPatientInfo(@RequestParam("patientId") int key){
+        Patient patientInfo = patientDb.get(key);
+        return patientInfo;
+    }
+}
---
+++ src/main/java/com/accio/Hospital/Management/System/Patient.java
@@ -0,0 +1,40 @@
+package com.accio.Hospital.Management.System;
+
+public class Patient {
+    private int patientId;
+    private String patientName;
+    private String disease;
+    private Long mobileNumber;
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public String getDisease() {
+        return disease;
+    }
+
+    public void setDisease(String disease) {
+        this.disease = disease;
+    }
+
+    public Long getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(Long mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+}
